bd03a068003d82a65039564c23edd439919a1d6b
henry <henryka@shrinqghana.com>
2023-07-18 22:11:22 -0700

    0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,10 +3,16 @@
 
 /**
  * main - Entry point
+ *
+ * Return: Always 0.
  */
 
 int main(void)
 {
-	putchar('\n');
+	_putchar('m');
+	_putchar('a');
+	_putchar('i');
+	_putchar('n');
+	_putchar('\n');
 	return (0);
 }
